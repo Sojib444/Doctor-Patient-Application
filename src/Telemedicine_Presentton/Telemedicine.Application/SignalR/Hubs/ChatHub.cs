23074b15ded7f5d3b442cf45f8dd1523905a0672
sojib <mdsojibhosen444@gmail.com>
2024-01-17 14:47:35 +0600

    working

---
+++ src/Telemedicine_Presentton/Telemedicine.Application/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Telemedicine.Application.SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static List<string> Message = new List<string>();
+
+        public void SendMesssss(string mesage,string senderEmail, string receiverEmail)
+        {
+            //essage.Add($"{sender} send {mesage}");
+
+            Clients.All.SendAsync("ReceiveMassage", $"{senderEmail} send a message to {receiverEmail} and Messgae is {mesage}")
+                .GetAwaiter().GetResult();
+        }
+    }
+}
